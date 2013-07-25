4d26173e0ac61ae96ec329c48ff8d774031cf9bb
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-25 03:26:30 -0300

    remove arg mirror on bin clone all

--- bin/clone-all.sh
+++ bin/clone-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GITHUB_ORGANIZATION="opps"
-GIT_CLONE_CMD="git clone --quiet --mirror --recursive "
+GIT_CLONE_CMD="git clone --quiet --recursive "
 REPOLIST=`curl --silent https://api.github.com/orgs/${GITHUB_ORGANIZATION}/repos -q | grep "\"ssh_url\"" | awk -F': "' '{print $2}' | sed -e 's/",//g'`
  
 for REPO in $REPOLIST; do

dd93ef414231419788db565af0b5636a5a853f8c
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-25 03:22:21 -0300

    Create bin file generate mirror

---
+++ bin/clone-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GITHUB_ORGANIZATION="opps"
+GIT_CLONE_CMD="git clone --quiet --mirror --recursive "
+REPOLIST=`curl --silent https://api.github.com/orgs/${GITHUB_ORGANIZATION}/repos -q | grep "\"ssh_url\"" | awk -F': "' '{print $2}' | sed -e 's/",//g'`
+ 
+for REPO in $REPOLIST; do
+    ${GIT_CLONE_CMD}${REPO}
+done
